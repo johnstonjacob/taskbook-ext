e5fae97da0a0f327ac3a0d811d88b3be81e8bdfe
Jacob Johnston <jacob@johnstonjacob.com>
2018-11-03 17:52:30 -0500

    remove commit logic

--- .circleci/version_check.sh
+++ .circleci/version_check.sh
@@ -1,11 +1,5 @@
 set -e
 
-# latest commit
-LATEST_COMMIT=$(git rev-parse HEAD)
-
-# latest commit where path/to/folder1 was changed
-LATEST_PACKAGE=$(git log -1 --format=format:%H --full-diff package.json)
-
 PACKAGE_VERSION=$(cat package.json \
   | grep version \
   | head -1 \
@@ -15,12 +9,10 @@ PACKAGE_VERSION=$(cat package.json \
 
 CURRENT_VERSION=$(npm view taskbook-ext version)
 
-if [ $LATEST_PACKAGE = $LATEST_COMMIT ]; then
-    if [ $PACKAGE_VERSION != $CURRENT_VERSION ]; then
-        echo "//registry.npmjs.org/:_authToken=$npm_TOKEN" >> ~/repo/.npmrc
-        echo "Version has changed, releasing"
-        npm publish
-    fi
+if [ $PACKAGE_VERSION != $CURRENT_VERSION ]; then
+    echo "//registry.npmjs.org/:_authToken=$npm_TOKEN" >> ~/repo/.npmrc
+    echo "Version has changed, releasing"
+    npm publish
 else
      echo "No version increment"
      exit 0;
